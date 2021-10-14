47bba5f9bc0243a31fac8b7030fb54fa6cc599d3
mdnahid26 <naimulnihal2696@gmail.com>
2021-10-15 00:19:12 +0600

    first upload

---
+++ assets/js/app.js
@@ -0,0 +1,69 @@
+$(function(){
+    'use stirct';
+    //
+    $('.search_btn').on('click',function(){
+        $('.search_overly').addClass('search_active')
+    });
+    $('.close_icon').on('click',function(){
+        $('.search_overly').removeClass('search_active')
+    })
+
+    //slick
+    $('.slick_slider').slick({
+        peed: 500,
+        fade: true,
+        prevArrow:'<i class="fas fa-chevron-left slider_arrow slider_left"></i>',
+        nextArrow:'<i class="fas fa-chevron-right slider_arrow slider_right"></i>',
+    })
+    $('.client_slick').slick({
+        arrows:false,
+        slidesToShow:5,
+        infinite:false,
+        responsive:[
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow:4,
+                }
+              },
+              {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow:3,
+                }
+              },
+              {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow:2,
+                }
+              },
+            ]
+    })
+    $('.testimonial_slick').slick({
+        arrows:false,
+        dots:true,
+        dotsClass:'test_dots',
+    })
+    //venobox
+    $('.venobox').venobox();
+
+    //counter up
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+    //backToTop bottom
+    $(window).on('scroll',function(){
+        if($(window).scrollTop() > 400){
+            $('#backToTop').fadeIn('slow')
+        }else{
+            $('#backToTop').fadeOut('slow')
+        }
+    })
+
+    $('#backToTop').on('click',function(){
+        $('html,body').scrollTop(0)
+    })
+})
